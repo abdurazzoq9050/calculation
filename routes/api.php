a2bd81c00fc01897e2141d209d07da1fc3740737
Your Name <you@example.com>
2025-04-13 17:59:29 +0300

    delete register

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [UserController::class, 'login']);
-Route::post('/register', [UserController::class, 'store']);
+// Route::post('/register', [UserController::class, 'store']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('user', [UserController::class, 'show']);
